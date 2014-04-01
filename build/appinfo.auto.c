dfa71904c90d3dff2366f5d317ba97c8b61eea7a
Wolfgang Meyerle <procrash@neusob.de>
2014-04-01 20:20:31 +0100

    Second commit

---
+++ build/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_app_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleAppInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { APP_INFO_CURRENT_STRUCT_VERSION_MAJOR, APP_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { APP_INFO_CURRENT_SDK_VERSION_MAJOR, APP_INFO_CURRENT_SDK_VERSION_MINOR },
+  .app_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Xperience",
+  .company = "Procrash",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = APP_INFO_WATCH_FACE,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x3C, 0xC5, 0xB0, 0x16, 0x3B, 0x2E, 0x4C, 0x12, 0xA8, 0xF2, 0xF8, 0x4E, 0x52, 0xF7, 0xFD, 0x39 },
+  .virtual_size = 0xb6b6
+};
